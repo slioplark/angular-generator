3d8b5c30b2292bf384ccae77004da418323db6a8
slioplark <jeailfy9520@outlook.com>
2019-09-21 17:03:31 +0800

    feat: add generator table html code

--- src/app/pages/material/table/table.component.ts
+++ src/app/pages/material/table/table.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  genCode: string;
+  inputValue: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  generator() {
+    this.genCode = '';
+    const colList = this.inputValue ? this.inputValue.split(',').map(item => item.trim()) : [];
+    colList.forEach(item => {
+      this.genCode += `
+      <!-- ${item} Column -->
+      <ng-container matColumnDef="${item}">
+        <mat-header-cell *matHeaderCellDef>${item}</mat-header-cell>
+        <mat-cell *matCellDef="let row">{{ row.${item} }}</mat-cell>
+      </ng-container>
+      `;
+    });
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    FormsModule
   ],
   exports: [
     CommonModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    FormsModule
   ]
 })
 export class SharedModule { }
