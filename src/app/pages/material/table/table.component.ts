be2d8585ed61659ab064d21f61a25003aa3a1e01
Mason <mason.chang@cimforce.com>
2019-09-23 18:10:10 +0800

    fix: remove module

--- src/app/pages/material/table/table.component.ts
+++ src/app/pages/material/table/table.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-table',
