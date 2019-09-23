0bf481aa3f3e47be52ae73c6b18e6ab4e0533674
Mason <mason.chang@cimforce.com>
2019-09-23 14:20:47 +0800

    feat: add generator method

--- src/app/pages/material/autocomplete/autocomplete.component.ts
+++ src/app/pages/material/autocomplete/autocomplete.component.ts
@@ -8,6 +8,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AutocompleteComponent implements OnInit {
 
+  formName: string;
+  autoName: string;
+
   codeHtml: string;
   codeTypescript: string;
 
@@ -24,7 +27,41 @@ export class AutocompleteComponent implements OnInit {
   }
 
   onCreate() {
+    this.formName = this.form.get('column').value.trim();
+    this.autoName = this.formName[0].toUpperCase() + this.formName.slice(1);
+    this.getHtml();
+    this.getTypescript();
+  }
 
+  getHtml() {
+    this.codeHtml = `
+    <mat-form-field>
+      <input type="text" matInput formControlName="${this.formName}" [matAutocomplete]="auto${this.autoName}">
+      <mat-autocomplete #auto${this.autoName}="matAutocomplete" [displayWith]="display${this.autoName}Fn">
+        <mat-option *ngFor="let item of list$ | async" [value]="item">
+          {{ item.name }}
+        </mat-option>
+      </mat-autocomplete>
+    </mat-form-field>
+    `;
+  }
+
+  getTypescript() {
+    this.codeTypescript = `
+    ngOnInit() {
+      this.list$ = this.form.get('${this.formName}').value.valueChanges
+        .pipe(
+          startWith<string | any>(''),
+          map(value => typeof value === 'string' ? value : value.name),
+          map(name => this.filter(name))
+        );
+    }
+    `;
+    this.codeTypescript += `
+    display${this.autoName}Fn(value?: any): string | undefined {
+      return value ? value.name : undefined;
+    }
+    `;
   }
 
 }
