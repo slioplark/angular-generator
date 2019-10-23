47c103924bdf78edb8140843d835b4c013841424
Mason <mason.chang@cimforce.com>
2019-10-23 18:24:54 +0800

    feat: add generator typescript method

--- src/app/pages/angular/life-cycle/life-cycle.component.ts
+++ src/app/pages/angular/life-cycle/life-cycle.component.ts
@@ -7,9 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LifeCycleComponent implements OnInit {
 
+  codeTypescript: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.genTypescript();
+  }
+
+  genTypescript() {
+    this.codeTypescript = `
+    export class LifeCycleComponent implements OnChanges,
+                                               OnInit, DoCheck,
+                                               AfterContentInit, AfterContentChecked,
+                                               AfterViewInit, AfterViewChecked,
+                                               OnDestroy {
+    `;
+    this.codeTypescript += `
+      ngOnChanges(changes: SimpleChanges) {
+        console.log('OnChanges');
+      }
+    `;
+    this.codeTypescript += `
+      ngOnInit() {
+        console.log('OnInit');
+      }
+    `;
+    this.codeTypescript += `
+      ngDoCheck() {
+        console.log('DoCheck');
+      }
+    `;
+    this.codeTypescript += `
+      ngAfterContentInit() {
+        console.log('AfterContentInit');
+      }
+    `;
+    this.codeTypescript += `
+      ngAfterContentChecked () {
+        console.log('AfterContentChecked');
+      }
+    `;
+    this.codeTypescript += `
+      ngAfterViewInit() {
+        console.log('AfterViewInit');
+      }
+    `;
+    this.codeTypescript += `
+      ngAfterViewChecked() {
+        console.log('AfterViewChecked');
+      }
+    `;
+    this.codeTypescript += `
+      ngOnDestroy() {
+        console.log('OnDestroy');
+      }
+    `;
+    this.codeTypescript += `
+    }
+    `;
   }
 
 }
